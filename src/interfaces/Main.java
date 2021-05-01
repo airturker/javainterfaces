95d3cad95cf39acc3d21fbb9d2112267c1ca56a2
Mehmet Türker ÖZAKINCI <airtur@hotmail.com>
2021-05-02 00:01:21 +0300

    1 mayıs ekleme

--- src/interfaces/Main.java
+++ src/interfaces/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Logger[] loggers = {new SmsLogger(), new EmailLogger(), new FileLogger()}; //istediðini bu array e ekle
+		Logger[] loggers = {new SmsLogger(), new EmailLogger(), new FileLogger()}; //istediðini bu array e ekle - diziye
 		
 		CustomerManager customerManager = new CustomerManager(loggers);
 		
